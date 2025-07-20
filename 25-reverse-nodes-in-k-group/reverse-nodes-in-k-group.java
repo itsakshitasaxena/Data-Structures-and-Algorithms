1e386b851801dc81eaca1bbdc30150cce32e1d0f
Akshita Saxena <166048233+itsakshitasaxena@users.noreply.github.com>
2025-07-20 17:16:08 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (43.53%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public ListNode get_kthnode(ListNode temp, int k){
-        k-=1;
+        k-=1; //Decrement k as we already start from the 1st node
         while(temp!=null && k>0){
             k--;
             temp=temp.next;
@@ -21,20 +21,23 @@ class Solution {
         ListNode temp=head, prev=null;
         while(temp!=null){
             ListNode kth_node=get_kthnode(temp, k); //everytime get starting point of group
-            if(kth_node==null){
-                if(prev!=null)   prev.next=temp;
+            if(kth_node==null){ //(not a complete group)
+                if(prev!=null)   
+                    prev.next=temp; //link the last node to the current node
                 break;
             }
 
             ListNode nextnode=kth_node.next;
-            kth_node.next=null;
+            kth_node.next=null; //Disconnect the kth node
             reverse(temp);
 
             if(temp==head)  head=kth_node;
-            else   prev.next=kth_node;
+            else   
+                prev.next=kth_node; //Link the last node of the previous
+                // group to the reversed group
 
-            prev=temp;
-            temp=nextnode;
+            prev=temp; // Update the pointer to the last node of the previous group
+            temp=nextnode; //  Move to the next group
         }
         return head;
     }
