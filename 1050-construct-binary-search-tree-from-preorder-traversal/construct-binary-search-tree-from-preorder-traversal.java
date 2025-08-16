4a3bde99b54145eff739853e88b53ddd2ef34a1c
Akshita Saxena <166048233+itsakshitasaxena@users.noreply.github.com>
2025-08-16 15:39:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (55.35%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.java
@@ -20,12 +20,15 @@ class Solution {
         return constructBT(preorder,Integer.MAX_VALUE);
     }
     public TreeNode constructBT(int[] preorder,int bound){
+        
         if(i==preorder.length || preorder[i] >bound)     return null;       
 
         TreeNode node=new TreeNode(preorder[i++]);
         
         node.left=constructBT(preorder,node.val);
+        //for left, node is value of that node(all leftie less than node val)
         node.right=constructBT(preorder,bound);
+        //for right, max value is bound as all rightie greater than nodeval
         return node;
     }
 }
