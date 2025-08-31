7dcfa084b6488c303b9d2d4a8a9846c7cadb63ff
Akshita Saxena <166048233+itsakshitasaxena@users.noreply.github.com>
2025-08-31 15:59:48 +0530

    Time: 41 ms (13.07%) | Memory: 61.3 MB (67.56%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -4,16 +4,15 @@ class Solution {
         for(int i:nums)
             set.add(i);
         int ans=0;
+    //"Start only when it’s the beginning. Grow the sequence. Track the max."
         for(int i=0;i<nums.length;i++){
             if(!set.contains(nums[i]-1)){
-                int cnt=0,key=nums[i];
+                int key=nums[i],cnt=0;
                 while(set.contains(key)){
                     set.remove(key);
-                    key++;cnt++;
-                    
+                    key++; cnt++;
                 }
-                ans=Math.max(cnt,ans);
-
+                ans=Math.max(ans,cnt);
             }
         }
         return ans;
