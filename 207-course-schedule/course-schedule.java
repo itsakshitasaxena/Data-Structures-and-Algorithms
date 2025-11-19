e9988b2a8eabeac73bb214bd9998d3d96a77a282
Akshita Saxena <166048233+itsakshitasaxena@users.noreply.github.com>
2025-11-19 21:43:43 +0530

    Time: 6 ms (73.95%) | Memory: 47.2 MB (9.94%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        //Topological Sort--> only used for Directed Acyclic Graph
+        //here, we did by using BFS i.e. Kahn's Algorithm
+        //Topological sort is not possible if there exists a cycle
+
+        //build adjacency list
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<numCourses;i++)
+            adj.add(new ArrayList<>());
+        for(int[] courses:prerequisites)
+            adj.get(courses[1]).add(courses[0]);
+
+        //built indegree array
+        int[] in=new int[numCourses];
+        for(int[] course:prerequisites)
+            in[course[0]]++;
+        Queue<Integer> qu=new LinkedList<>();
+        List<Integer> ord=new ArrayList<>();
+        for(int i=0;i<numCourses;i++) if(in[i]==0)  qu.add(i);
+        while(!qu.isEmpty()){
+            int r=qu.poll();
+            ord.add(r);
+            for(int nbrs:adj.get(r)){
+                in[nbrs]--;
+                if(in[nbrs]==0)  qu.add(nbrs);
+            }
+
+        }
+        return ord.size()==numCourses; //checking for cycle
+    }
+}
