32258483c7beceeb06ffb5a871176a5fed0e88aa
Akshita Saxena <166048233+itsakshitasaxena@users.noreply.github.com>
2026-01-06 12:39:28 +0530

    Time: 13 ms (10.17%) | Memory: 65.8 MB (10.56%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,38 +1,29 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<String> ll = new ArrayList<>();
-        List<List<String>> res = new ArrayList<>();
-        partition(s,"", ll, res);
-        return res;
+        List<List<String>> ans=new ArrayList<>();
+        List<String> ll=new ArrayList<>();
+        partition(ans,ll,s,"");
+        return ans;
     }
-    public static void partition(String ques, String ans, List<String> ll, List<List<String>> res)
-    { 
-        if(ques.length()==0)
-        {
-            res.add(new ArrayList(ll));
-                // System.out.println(ans);
-                // return;
+    public void partition(List<List<String>> ans, List<String> ll, String ques, String st){
+        if(ques.length()==0){
+            ans.add(new ArrayList<>(ll));
             return;
         }
         for(int cut=1;cut<=ques.length();cut++){
-            String s=ques.substring(0,cut);
-            if(ispalindrome(s))
-            {
-                ll.add(s);
-                partition(ques.substring(cut), ans+s+"|",ll,res);
+            String sub=ques.substring(0,cut);
+            if(ispalindrome(sub)){
+                ll.add(sub);
+                partition(ans,ll,ques.substring(cut),st+sub);
                 ll.remove(ll.size()-1);
             }
         }
-
     }
-    public static boolean ispalindrome(String st){
-        int i=0,j=st.length()-1;
-        while(i<j)
-        {
-            if(st.charAt(i)!=st.charAt(j))
-              return false;
-            i++;
-            j--;
+    public boolean ispalindrome(String st){
+        int i=0, j=st.length()-1;
+        while(i<=j){
+            if(st.charAt(i)!=st.charAt(j)) return false;
+            i++;j--;
         }
         return true;
     }
