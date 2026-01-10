d0cd4d5f9c8339bbe535765576d24f2c9a8836a5
Akshita Saxena <166048233+itsakshitasaxena@users.noreply.github.com>
2026-01-10 22:27:16 +0530

    Time: 1 ms (100.00%) | Memory: 45.4 MB (48.86%) - LeetSync

--- 2754-maximum-strength-of-a-group/maximum-strength-of-a-group.java
+++ 2754-maximum-strength-of-a-group/maximum-strength-of-a-group.java
@@ -1,23 +1,35 @@
 class Solution {
     public long maxStrength(int[] nums) {
         if(nums.length==1) return nums[0];
-        long pro=1,min=Integer.MIN_VALUE,zero=0;
-        Arrays.sort(nums);
-        boolean z=false;
-        for(int i:nums){
-            if(i>0) {
-                pro*=i;
-                z=true;
+        long pro=1,min=Integer.MIN_VALUE,zero=0,ans=1;
+        for(int i=0 ; i<nums.length ; i++){
+            if(nums[i]==0){
+                zero++;
+                continue;
             }
+            if(nums[i]<0) min = Math.max(min,nums[i]);
+            ans *= nums[i];
         }
-        for(int i=0;i+1<nums.length;i++){
-            if(nums[i]<0 && nums[i+1]<0){ //multiply -ves in pairs
-                pro*=(nums[i]*nums[i+1]);
-                i++;
-                z=true;
-            }
-        }
-        return z?pro:0;
+        if(zero==nums.length || (zero!=0 && zero==nums.length-1 && min!=Integer.MIN_VALUE)) return 0;
+        if(ans<0) return ans/min;
+        return ans;
         
     }
 }
+
+// Arrays.sort(nums);
+//         boolean z=false;
+//         for(int i:nums){
+//             if(i>0) {
+//                 pro*=i;
+//                 z=true;
+//             }
+//         }
+//         for(int i=0;i+1<nums.length;i++){
+//             if(nums[i]<0 && nums[i+1]<0){ //multiply -ves in pairs
+//                 pro*=(nums[i]*nums[i+1]);
+//                 i++;
+//                 z=true;
+//             }
+//         }
+//         return z?pro:0; 
