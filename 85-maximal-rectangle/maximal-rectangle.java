4e0527c1b6a933239d63b85c4f27a4c8a87002a6
Akshita Saxena <166048233+itsakshitasaxena@users.noreply.github.com>
2026-01-11 09:06:06 +0530

    Time: 15 ms (59.33%) | Memory: 50.3 MB (13.47%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,43 +1,43 @@
 class Solution {
-    public int maximalRectangle(char[][] matrix) {
-        if(matrix.length==0)   return 0;
-        int[] prefix=new int[matrix[0].length];
-        int maxarea=0;
-        for(int i=0;i<matrix.length;i++){
-            int sum=0;
-            for(int j=0;j<matrix[0].length;j++){
-                // sum+=matrix[i][j];
-                if(matrix[i][j]=='0')
-                    prefix[j]=0;
-                else
-                    prefix[j]+=1;
+    public int maximalRectangle(char[][] ar) {
+        int[] h=new int[ar[0].length];
+        int largest=0;
+        for(int i=0;i<ar.length;i++){
+            for(int j=0;j<ar[0].length;j++){
+                h[j]=ar[i][j]=='0'?0:h[j]+(ar[i][j]-'0');
             }
-            maxarea=Math.max(maxarea, largestRectangleArea(prefix));
+            largest=Math.max(largest, largestRectangleArea(h));
         }
-        return maxarea;
+        return  largest;
     }
     public int largestRectangleArea(int[] heights) {
-// brute force is to find nse and pse array for each ele of heights and applying the same formula i.e. max(maxarea, ar[i]*(nse[i]-pse[i]-1)) but in this, TC-O(5N) SC-O(4N) so, to optimize this we will find nse and pse in the same loop
-        // formula to solve this maxarea=
-        // max(maxarea, ar[i]*(nse ka index -pse ka index -1))
-        // TC-O(2N), SC-O(N) optimised sol.n
         Stack<Integer> st=new Stack<>();
-        int max_area=0;
+        int max=0;
         for(int i=0;i<heights.length;i++){
-            while(!st.isEmpty() && heights[st.peek()]>heights[i]){
-                int ele=st.peek();
-                st.pop();
-                int nse=i , pse=st.isEmpty()?-1:st.peek();
-                max_area=Math.max(max_area, heights[ele]*(nse-pse-1));
+            while(!st.isEmpty() && heights[i]<heights[st.peek()]){ //jis value pr hu vo choti honi chahiye peek se
+            int h=heights[st.pop()]; //pop krenge kuki iss height ki ab need ni h future m
+            int r=i; //jahan pr abhi hu wahan right pointer rahega
+            if(st.isEmpty()){
+                max=Math.max(max,h*r);
+            }
+            else{
+                int l=st.peek();
+                max=Math.max(max,h*(r-l-1));
+            }
             }
             st.push(i);
         }
+        int r=heights.length;
         while(!st.isEmpty()){
-            int ele=st.peek();   
-            st.pop();
-            int nse=heights.length, pse=st.isEmpty()?-1:st.peek();
-            max_area=Math.max(max_area, heights[ele]*(nse-pse-1));
+            int h=heights[st.pop()]; //pop krenge kuki iss height ki ab need ni h future m
+            if(st.isEmpty()){
+                max=Math.max(max,h*r);
+            }
+            else{
+                int l=st.peek();
+                max=Math.max(max,h*(r-l-1));
+            }
         }
-        return max_area;
+        return max;
     }
 }
