213432bf2da10128173d5b879121a33a27904d11
Akshita Saxena <166048233+itsakshitasaxena@users.noreply.github.com>
2026-01-16 12:15:36 +0530

    Time: 1 ms (99.56%) | Memory: 49.3 MB (8.85%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,31 +1,23 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-        int i = 0, j = 0, m1 = 0, m2 = 0;
-
-        // Find median.
-        for (int count = 0; count <= (n + m) / 2; count++) {
-            m2 = m1;
-            if (i != n && j != m) {
-                if (nums1[i] > nums2[j]) {
-                    m1 = nums2[j++];
-                } else {
-                    m1 = nums1[i++];
-                }
-            } else if (i < n) {
-                m1 = nums1[i++];
-            } else {
-                m1 = nums2[j++];
+        if(nums1.length>nums2.length)  return findMedianSortedArrays(nums2, nums1);
+        int m=nums1.length, n=nums2.length;
+        int lo=0, hi=m;
+        while(lo<=hi){
+            int cut1=(lo+hi)/2;
+            int cut2=(m+n+1)/2-cut1;
+            int l1= cut1==0?Integer.MIN_VALUE:nums1[cut1-1];
+            int l2= cut2==0?Integer.MIN_VALUE:nums2[cut2-1];
+            int r1= cut1==m?Integer.MAX_VALUE:nums1[cut1];
+            int r2= cut2==n?Integer.MAX_VALUE:nums2[cut2];
+            if(l1<=r2 && l2<=r1){
+                if((m+n)%2==0)
+                    return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+                else return Math.max(l1,l2);
             }
+            else if(l1>r2)  hi=cut1-1;
+            else lo=cut1+1;
         }
-
-        // Check if the sum of n and m is odd.
-        if ((n + m) % 2 == 1) {
-            return (double) m1;
-        } else {
-            double ans = (double) m1 + (double) m2;
-            return ans / 2.0;
-        }
+        return 0.0;
     }
 }
