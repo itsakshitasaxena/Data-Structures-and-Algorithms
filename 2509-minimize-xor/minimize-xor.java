a739da2434e79587cca791d9df45471d82a48fee
Akshita Saxena <166048233+itsakshitasaxena@users.noreply.github.com>
2026-01-21 12:39:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (50.63%) - LeetSync

---
+++ 2509-minimize-xor/minimize-xor.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minimizeXor(int num1, int num2) {
+        return find_x(num1, num2);
+    }
+    public int find_x(int num1, int num2){
+        int set_bit=0;
+        while(num2>0){
+            num2=(num2&(num2-1));
+            set_bit++;
+        }
+        int x=0;
+        for(int i=30;i>=0;i--){
+            int mask=(1<<i); // 100000
+            if((num1&mask)!=0){
+                x|=mask; //x=x|mask;
+                set_bit--;
+                if(set_bit==0) return x;
+            }
+        }
+        for(int i=0;i<=30;i++){ //if set bit remains
+             int mask=(1<<i);
+            if((num1&mask)==0){
+                x|=mask; //x=x|mask;
+                set_bit--;
+                if(set_bit==0) return x;
+            }
+        }
+        return x;
+    }
+}
