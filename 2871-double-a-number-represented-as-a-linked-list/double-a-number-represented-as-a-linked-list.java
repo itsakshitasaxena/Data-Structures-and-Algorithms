17c5e6af37c64f9b88a57e85aa3269e1cca97ef1
Akshita Saxena <166048233+itsakshitasaxena@users.noreply.github.com>
2026-02-02 15:23:00 +0530

    Time: 4 ms (81.48%) | Memory: 48.3 MB (82.44%) - LeetSync

---
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode doubleIt(ListNode head) {
+        head=reverse(head);
+        return reverse(addTwoNumbers(head,head));
+    }
+    public ListNode reverse(ListNode head){
+        ListNode curr=head, prev=null;
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy=new ListNode(0);
+        ListNode curr=dummy, head1=  l1, head2=l2;
+        int carry=0;
+        while(head1!=null || head2!=null || carry!=0){
+            int a=(head1!=null) ? head1.val : 0;
+            int b=(head2!=null) ? head2.val : 0;
+            int sum=a+b+carry;
+            carry=sum/10;
+            curr.next=new ListNode(sum%10);
+            curr=curr.next;
+            if(head1!=null)    head1=head1.next;
+            if(head2!=null)    head2=head2.next;
+        }
+        return dummy.next;
+    }
+}
